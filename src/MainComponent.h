2c7c8884c46eefe97a98f4b2c470c54bfa3a6be5
Chikakoo <lionel.crafts@gmail.com>
2024-11-21 12:34:38 -0600

    Audo-format files to be modified later

--- src/MainComponent.h
+++ src/MainComponent.h
@@ -3,20 +3,20 @@
  *
  * MainComponent.h
  * GUI component that holds the menus, tabs, etc. and performs menu functions
- * 
+ *
  * From seq64 - Sequenced music editor for first-party N64 games
  * Copyright (C) 2014-2019 Sauraen
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * ============================================================================
@@ -40,56 +40,56 @@ class Finder;
 */
 
 
-class MainComponent    : public Component,
-                         public MenuBarModel
+class MainComponent : public Component,
+	public MenuBarModel
 {
 public:
-    MainComponent(SEQ64& seq64_, DocumentWindow& window_);
-    ~MainComponent();
-    
-    StringArray getMenuBarNames() override;
-    PopupMenu getMenuForIndex(int topLevelMenuIndex, const String &menuName) override;
-    void menuItemSelected(int menuItemID, int topLevelMenuIndex) override;
+	MainComponent(SEQ64& seq64_, DocumentWindow& window_);
+	~MainComponent();
 
-    void queryByteOrdering();
-    void doCodecOperation(int operation);
-    
-    void paint (Graphics&);
-    void resized();
-    
-    void actuallySaveROM();
-    void actuallySaveRomDesc();
-    
-    void onROMLoaded();
-    void onRomDescLoaded();
-    void onSeqLoaded();
-    void onBankLoaded();
-    void onGotABI();
+	StringArray getMenuBarNames() override;
+	PopupMenu getMenuForIndex(int topLevelMenuIndex, const String& menuName) override;
+	void menuItemSelected(int menuItemID, int topLevelMenuIndex) override;
+
+	void queryByteOrdering();
+	void doCodecOperation(int operation);
+
+	void paint(Graphics&);
+	void resized();
+
+	void actuallySaveROM();
+	void actuallySaveRomDesc();
+
+	void onROMLoaded();
+	void onRomDescLoaded();
+	void onSeqLoaded();
+	void onBankLoaded();
+	void onGotABI();
 
 private:
-    SEQ64& seq64;
-    DocumentWindow& window;
-    
-    StringArray menunames;
-    OwnedArray<PopupMenu> menus;
-    OwnedArray<PopupMenu> submenus;
+	SEQ64& seq64;
+	DocumentWindow& window;
 
-    std::unique_ptr<MenuBarComponent> mnuBar;
-    std::unique_ptr<TabbedComponent> tabbox;
-    
-    std::unique_ptr<FilesPane> filespane;
-    std::unique_ptr<AudioseqPane> audioseqpane;
-    std::unique_ptr<MidiPane> midipane;
-    std::unique_ptr<AudiobankPane> audiobankpane;
-    std::unique_ptr<Finder> finder;
+	StringArray menunames;
+	OwnedArray<PopupMenu> menus;
+	OwnedArray<PopupMenu> submenus;
 
-    int menuBarHeight;
-    
-    const static String helpText1, helpText2, helpText2b, helpText3;
-    
-    std::unique_ptr<Yax0Codec> codec;
+	std::unique_ptr<MenuBarComponent> mnuBar;
+	std::unique_ptr<TabbedComponent> tabbox;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+	std::unique_ptr<FilesPane> filespane;
+	std::unique_ptr<AudioseqPane> audioseqpane;
+	std::unique_ptr<MidiPane> midipane;
+	std::unique_ptr<AudiobankPane> audiobankpane;
+	std::unique_ptr<Finder> finder;
+
+	int menuBarHeight;
+
+	const static String helpText1, helpText2, helpText2b, helpText3;
+
+	std::unique_ptr<Yax0Codec> codec;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
 };
 
 
